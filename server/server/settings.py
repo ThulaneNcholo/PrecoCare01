0a4952e0076255b89180dab633a537af9d749d06
thulane <thulanencholo95@gmail.com>
2022-09-21 11:05:34 +0200

    init

--- server/server/settings.py
+++ server/server/settings.py
@@ -129,6 +129,10 @@ STATICFILES_DIRS = [
     '/var/www/static/',
 ]
 
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"
+# ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
